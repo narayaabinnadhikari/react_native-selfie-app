353bc1e9e30438d70b529b645891a8be6ca5ddb7
Narayan Adhikari <narayanabin223@gmail.com>
2025-05-27 14:01:33 +0545

    02 different styling techniques

--- app/index.jsx
+++ app/index.jsx
@@ -1,15 +1,35 @@
 //rnfes
-import { StyleSheet, Text, View } from 'react-native'
+import { Image, StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import Logo from '../assets/img/logo_light.png'
 
 const Home = () => {
   return (
-    <View>
-      <Text>Home</Text>
+    <View style={styles.container}>
+        <Image source={Logo} style={styles.logo} />
+      <Text style={[styles.title, {color: 'green'}]}>Home</Text>
+      <Text style={{color: 'red', marginTop: 10, marginBottom: 20}}>This looks like a flexbox </Text>
     </View>
   )
 }
 
 export default Home
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    logo: {
+        marginVertical: 20,
+        borderColor: 'maroon',
+        borderWidth: 5,
+        borderRadius: 10
+
+    },
+    title: {
+        fontWeight: 'bold',
+        fontSize: 18,
+    },
+})

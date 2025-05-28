a8447166a68d56af4674f956e17dacbd914b6124
Narayan Adhikari <narayanabin223@gmail.com>
2025-05-28 10:55:49 +0545

    05 Light and Dark Theme

---
+++ constants/Colors.js
@@ -0,0 +1,22 @@
+export const Colors = {
+    primary: '#1976D2',
+    warning: '#FFA000',
+    dark: {
+        text: '#FFFFFF',
+        title: '#E0E0E0',
+        background: '#121212',
+        navBackground: '#1F1F1F',
+        iconColor: '#B0BEC5',
+        uiBackground: '#1F1F1F',
+        border: '#1F1F1F',
+    },
+    light: {
+        text: '#000000',
+        title: '#212121',
+        background: '#FAFAFA',
+        navBackground: '#F5F5F5',
+        iconColor: '#757575',
+        uiBackground: '#FFFFFF',
+        border: '#FFFFFF',
+    },
+    }
